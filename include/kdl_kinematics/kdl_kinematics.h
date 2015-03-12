ee612fe76d5b5de511575179407cd9ed3690be25
Gennaro Raiola <gennaro.raiola@gmail.com>
2015-03-12 10:09:49 +0100

    Moved Eigen macros to the test cpp

--- include/kdl_kinematics/kdl_kinematics.h
+++ include/kdl_kinematics/kdl_kinematics.h
@@ -4,15 +4,6 @@
 ////////// ROS
 #include "ros/ros.h"
 
-#ifdef EIGEN_MALLOC_CHECKS
-  #define EIGEN_RUNTIME_NO_MALLOC
-  #define START_REAL_TIME_CRITICAL_CODE() do { Eigen::internal::set_is_malloc_allowed(false); } while (0) 
-  #define END_REAL_TIME_CRITICAL_CODE() do { Eigen::internal::set_is_malloc_allowed(true); } while (0) 
-#else
-  #define START_REAL_TIME_CRITICAL_CODE() do {  } while (0) 
-  #define END_REAL_TIME_CRITICAL_CODE() do {  } while (0) 
-#endif
-
 ////////// Eigen
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/SVD>
--- test/test_kdl_kinematics.cpp
+++ test/test_kdl_kinematics.cpp
@@ -1,3 +1,12 @@
+#ifdef EIGEN_MALLOC_CHECKS
+  #define EIGEN_RUNTIME_NO_MALLOC
+  #define START_REAL_TIME_CRITICAL_CODE() do { Eigen::internal::set_is_malloc_allowed(false); } while (0) 
+  #define END_REAL_TIME_CRITICAL_CODE() do { Eigen::internal::set_is_malloc_allowed(true); } while (0) 
+#else
+  #define START_REAL_TIME_CRITICAL_CODE() do {  } while (0) 
+  #define END_REAL_TIME_CRITICAL_CODE() do {  } while (0) 
+#endif
+
 #include <gtest/gtest.h>
 #include "kdl_kinematics/kdl_kinematics.h"
 
